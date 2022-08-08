62b11aed74a32c0a4432d30b8ad81b422b890ba3
Daniel Charry <dacharry93@gmail.com>
2022-08-08 13:01:00 -0700

    Create base project

---
+++ BreakTimeRimder/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BreakTimeRimder
+//
+//  Created by Daniel on 8/08/22.
+//  Copyright © 2022 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello BreakTimeRimder")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
